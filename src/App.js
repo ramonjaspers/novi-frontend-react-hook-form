d0c9b5d96b6391a3e8c066d76cd2632d37ba1843
ramonjaspers <ramonj360@gmail.com>
2021-09-19 17:55:53 +0200

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import './App.css';
+import { useForm } from 'react-hook-form';
 
-function App() {
+export default function App() {
+  const { register, handleSubmit, formState: { errors }, watch } = useForm({ mode: 'onChange' });
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+
+  const selectedReferrer = watch('Bezorgfrequentie');
+
   return (
-    <div>
-      Bouw hier jouw React Hook Form!
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Voornaam" {...register("Voornaam", { required: "Voornaam is verplicht", maxLength: 80 })} />
+      {errors.Voornaam && <span>{errors.Voornaam.message}</span>}<br />
+      <input type="text" placeholder="Achternaam" {...register("Achternaam", { required: "Achternaam is verplicht", maxLength: 100 })} />
+      {errors.Achternaam && <span>{errors.Achternaam.message}</span>} <br />
+      <input type="number" placeholder="Leeftijd" {...register("Leeftijd", {
+        required: "Leeftijd is verplicht",
+        min: {
+          value: 18,
+          message: "Leeftijd moet hoger zijn dan 18"
+        },
+        pattern: /^\S+@\S+$/i
+      })} />
+      {errors.Leeftijd && <span>{errors.Leeftijd.message}</span>} <br />
+      <input type="text" placeholder="Postcode" {...register("Postcode", { required: "Postcode is verplicht", pattern: {value: /^\d{4}?[a-z]{2}$/i, message: "Invalide postcode"}})} />
+      {errors.Postcode && <span>{errors.Postcode.message}</span>} <br />
+      <select {...register("Bezorgfrequentie", { required: "Een keuze is verplicht" })}>
+        <option value="iedere week">iedere week</option>
+        <option value="om de week">om de week</option>
+        <option value="iedere maand">iedere maand</option>
+        <option value="anders">anders</option>
+      </select>
+      {errors.Bezorgfrequentie && <span>{errors.Bezorgfrequentie.message}</span>} <br />
+
+      {selectedReferrer === "anders" &&
+      <>
+        <input
+          type="text"
+          placeholder="Andere frequentie als tekstveld" {...register(
+            "afat",
+            { required: "Veld is verplicht" }
+          )}
+        />
+        {errors.afat && <span>{errors.afat.message}</span>} <br />
+        </>
+      }
+
+      <input type="text" placeholder="Opmerkingen" {...register("Opmerkingen", {})} />
+      <input type="checkbox" placeholder="Akkoord met de voorwaarden als checkbox" {...register("voorwaarden", { required: "U dient akkoord te gaan met de voorwaarden" })} />
+        {errors.voorwaarden && <span>{errors.voorwaarden.message}</span>} <br />
+
+      <input type="submit" />
+    </form>
   );
 }
-
-export default App;
